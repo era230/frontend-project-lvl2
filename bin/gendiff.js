624843bc79a6062a3615f7a4f473d1a067a4ba05
Elvira Aitkulova <aitkulovael@gmail.com>
2022-06-27 15:39:57 +0300

    2 step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,5 +5,7 @@ const program = new Command();
 program
     .version('output the version number')
     .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format <type>', 'output format')
+    .arguments('<filepath1> <filepath2>')
 
 program.parse();
