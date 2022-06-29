8b8ab66919d636cc587a6b6b95dbdf4460994a8d
Elvira Aitkulova <aitkulovael@gmail.com>
2022-06-29 23:19:17 +0300

    step 3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+import gendiff from '../src/index.js';
 import { Command } from 'commander';
 const program = new Command();
 
@@ -7,5 +8,8 @@ program
     .description('Compares two configuration files and shows a difference.')
     .option('-f, --format <type>', 'output format')
     .arguments('<filepath1> <filepath2>')
+    .action((filepath1, filepath2) => {
+        gendiff(filepath1, filepath2);
+    });
 
 program.parse();
---
+++ src/index.js
@@ -0,0 +1,36 @@
+import { readFileSync } from 'fs';
+import _ from 'lodash';
+import path from 'path';
+import { cwd } from 'node:process';
+
+const gendiff = (filepath1, filepath2) => {
+    const normalizePath1 = path.resolve(cwd(), filepath1);
+    const normalizePath2 = path.resolve(cwd(), filepath2);
+    const obj1 = JSON.parse(readFileSync(normalizePath1, 'utf-8'));
+    const obj2 = JSON.parse(readFileSync(normalizePath2, 'utf-8'));
+    const keys1 = Object.keys(obj1)
+    const keys2 = Object.keys(obj2)
+    const keys = _.sortBy(_.uniq([...keys1, ...keys2]))
+    .reduce((acc, key) => {
+        if (keys1.includes(key) && keys2.includes(key) && obj1[key] === obj2[key]) {
+            acc.push(`   ${key}: ${obj1[key]}`);
+        }
+        if (keys1.includes(key) && keys2.includes(key) && obj1[key] !== obj2[key]) {
+            acc.push(` - ${key}: ${obj1[key]}`);
+            acc.push(` + ${key}: ${obj2[key]}`);
+        }
+        if (keys1.includes(key) && !keys2.includes(key)) {
+            acc.push(` - ${key}: ${obj1[key]}`);
+        }
+        if (!keys1.includes(key) && keys2.includes(key)) {
+            acc.push(` + ${key}: ${obj1[key]}`);
+        }
+        return acc;
+    }, []);
+    const gendiffStr = `{
+${keys.join('\n')}
+}`
+    console.log(gendiffStr);
+};
+
+export default gendiff;
