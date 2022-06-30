18b89ee2850d29dd718598b42711b855907d23c5
Elvira Aitkulova <aitkulovael@gmail.com>
2022-06-30 22:24:46 +0300

    add linter

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,16 @@
 #!/usr/bin/env node
-import gendiff from '../src/index.js';
 import { Command } from 'commander';
+import gendiff from '../src/index.js';
+
 const program = new Command();
 
 program
-    .version('output the version number')
-    .description('Compares two configuration files and shows a difference.')
-    .option('-f, --format <type>', 'output format')
-    .arguments('<filepath1> <filepath2>')
-    .action((filepath1, filepath2) => {
-        gendiff(filepath1, filepath2);
-    });
+  .version('output the version number')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    gendiff(filepath1, filepath2);
+  });
 
 program.parse();
--- src/index.js
+++ src/index.js
@@ -4,33 +4,33 @@ import path from 'path';
 import { cwd } from 'node:process';
 
 const gendiff = (filepath1, filepath2) => {
-    const normalizePath1 = path.resolve(cwd(), filepath1);
-    const normalizePath2 = path.resolve(cwd(), filepath2);
-    const obj1 = JSON.parse(readFileSync(normalizePath1, 'utf-8'));
-    const obj2 = JSON.parse(readFileSync(normalizePath2, 'utf-8'));
-    const keys1 = Object.keys(obj1)
-    const keys2 = Object.keys(obj2)
-    const keys = _.sortBy(_.uniq([...keys1, ...keys2]))
+  const normalizePath1 = path.resolve(cwd(), filepath1);
+  const normalizePath2 = path.resolve(cwd(), filepath2);
+  const obj1 = JSON.parse(readFileSync(normalizePath1, 'utf-8'));
+  const obj2 = JSON.parse(readFileSync(normalizePath2, 'utf-8'));
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+  const keys = _.sortBy(_.uniq([...keys1, ...keys2]))
     .reduce((acc, key) => {
-        if (keys1.includes(key) && keys2.includes(key) && obj1[key] === obj2[key]) {
-            acc.push(`   ${key}: ${obj1[key]}`);
-        }
-        if (keys1.includes(key) && keys2.includes(key) && obj1[key] !== obj2[key]) {
-            acc.push(` - ${key}: ${obj1[key]}`);
-            acc.push(` + ${key}: ${obj2[key]}`);
-        }
-        if (keys1.includes(key) && !keys2.includes(key)) {
-            acc.push(` - ${key}: ${obj1[key]}`);
-        }
-        if (!keys1.includes(key) && keys2.includes(key)) {
-            acc.push(` + ${key}: ${obj1[key]}`);
-        }
-        return acc;
+      if (keys1.includes(key) && keys2.includes(key) && obj1[key] === obj2[key]) {
+        acc.push(`   ${key}: ${obj1[key]}`);
+      }
+      if (keys1.includes(key) && keys2.includes(key) && obj1[key] !== obj2[key]) {
+        acc.push(` - ${key}: ${obj1[key]}`);
+        acc.push(` + ${key}: ${obj2[key]}`);
+      }
+      if (keys1.includes(key) && !keys2.includes(key)) {
+        acc.push(` - ${key}: ${obj1[key]}`);
+      }
+      if (!keys1.includes(key) && keys2.includes(key)) {
+        acc.push(` + ${key}: ${obj1[key]}`);
+      }
+      return acc;
     }, []);
-    const gendiffStr = `{
+  const gendiffStr = `{
 ${keys.join('\n')}
-}`
-    console.log(gendiffStr);
+}`;
+  console.log(gendiffStr);
 };
 
 export default gendiff;
