5cffc1c585d3d86b2844e2625cf5a7bac2e5e408
Elvira Aitkulova <aitkulovael@gmail.com>
2022-07-14 22:34:14 +0300

    6 step add findDiff

--- src/index.js
+++ src/index.js
@@ -4,36 +4,68 @@ import path from 'path';
 import { cwd } from 'node:process';
 import parse from './parsers.js';
 
-const getFixturePath = (filepath) => path.resolve(cwd(), filepath);
-const readFile = (filepath) => readFileSync(filepath, 'utf-8');
-const extension = (filepath) => path.extname(filepath).toLowerCase();
+const getObject = (filepath) => {
+  const getFixturePath = path.resolve(cwd(), filepath);
+  const data = readFileSync(getFixturePath, 'utf-8');
+  const extension = path.extname(filepath).toLowerCase();
+  const object = parse(data, extension);
+  return object;
+};
+
+const mkfile = (name, status = '', value = '') => ({
+  name, type: 'node-leaf', status, value,
+});
+
+const mkdir = (name, children = []) => ({
+  name, type: 'node-internal', children,
+});
+
+const getStatus = (key, object1, object2) => {
+  let result = '';
+  if (!Object.hasOwn(object1, key)) {
+    result = 'added';
+  } else if (!Object.hasOwn(object2, key)) {
+    result = 'deleted';
+  } else {
+    result = object1[key] === object2[key] ? 'unchanged' : 'changed';
+  }
+  return result;
+};
+
+const getValue = (status, key, object1, object2) => {
+  switch (status) {
+    case 'added':
+      return object2[key];
+    case 'deleted':
+      return object1[key];
+    case 'unchanged':
+      return object1[key];
+    case 'changed':
+      return { valueDel: object1[key], valueAdd: object2[key] };
+    default:
+      throw new Error(`Unknown value: ${status}`);
+  }
+};
 
 const gendiff = (filepath1, filepath2) => {
-  const obj1 = parse(readFile(getFixturePath(filepath1)), extension(filepath1));
-  const obj2 = parse(readFile(getFixturePath(filepath2)), extension(filepath2));
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
-  const keys = _.sortBy(_.uniq([...keys1, ...keys2]))
-    .reduce((acc, key) => {
-      if (keys1.includes(key) && keys2.includes(key) && obj1[key] === obj2[key]) {
-        acc.push(`   ${key}: ${obj1[key]}`);
-      }
-      if (keys1.includes(key) && keys2.includes(key) && obj1[key] !== obj2[key]) {
-        acc.push(` - ${key}: ${obj1[key]}`);
-        acc.push(` + ${key}: ${obj2[key]}`);
-      }
-      if (keys1.includes(key) && !keys2.includes(key)) {
-        acc.push(` - ${key}: ${obj1[key]}`);
-      }
-      if (!keys1.includes(key) && keys2.includes(key)) {
-        acc.push(` + ${key}: ${obj1[key]}`);
-      }
-      return acc;
-    }, []);
-  const gendiffStr = `{
-${keys.join('\n')}
-}`;
-  return gendiffStr;
+  const object1 = getObject(filepath1);
+  const object2 = getObject(filepath2);
+  const iter = (obj1, obj2) => {
+    const keys1 = Object.keys(obj1);
+    const keys2 = Object.keys(obj2);
+    const result = _.sortBy(_.union([...keys1, ...keys2]))
+      .map((key) => {
+        if (!_.isPlainObject(obj1[key]) || !_.isPlainObject(obj2[key])) {
+          const status = getStatus(key, obj1, obj2);
+          return mkfile(key, status, getValue(status, key, obj1, obj2));
+        }
+        return mkdir(key, iter(obj1[key], obj2[key]));
+      });
+    return result;
+  };
+  return iter(object1, object2);
 };
 
 export default gendiff;
+
+console.log(gendiff('../__fixtures__/file3.json', '../__fixtures__/file4.json'));
